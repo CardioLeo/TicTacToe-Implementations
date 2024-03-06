6e9565c9d8bafc8f971efece326d8842ad5610fd
echocslsk <CardioLeo@gmail.com>
2024-03-06 01:35:45 -0700

    updates lisp program with list of functions which were used in the C++ version of tictactoe

--- LispTicTacToe/tictactoe.lisp
+++ LispTicTacToe/tictactoe.lisp
@@ -2,8 +2,18 @@
   (print 'tic-tac-toe)
   (terpri))
 
+(defun my-ttt-list ()
+  (print '(ttt-global-vars (round_counter mark game_over play_again_input spot_to_fill board_spots)))
+  (terpri))
+
+(defun my-functions ()
+  (print '(ttt-functions (draw-board announce-three-in-a-row round-limit-checker display-info find-three-in-a-row change-turn attempt-to-fill-spot player-move start-game ask-to-play play-again reset-pregame-variables say-invaid)))
+  (terpri))
+
 (defun draw-board ()
   (tic-tac-toe)
+  (my-ttt-list)
+  (my-functions)
   (print'"
 
 
