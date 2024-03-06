d16cd1c71ef8189f3dfbfb18b0b763ea12c50c25
echocslsk <CardioLeo@gmail.com>
2024-03-06 00:06:00 -0700

    brings first, primitive version of print board to very rough working order for command line

--- LispTicTacToe/tictactoe.lisp
+++ LispTicTacToe/tictactoe.lisp
@@ -1,4 +1,8 @@
-: this is a comment (I think)
-:
+(defun tic-tac-toe ()
+  (print 'tic-tac-toe)
+  (terpri))
 
-(format t "test")
+(defun draw-board ()
+  (print '(     |     |     new-line  board_spots[0]  |  board_spots[1]  |  board_spots[2] new-line  _____|_____|_____ new-line     |     |    board_spots[3] new-line  |  board_spots[4]  |  board_spots[5]  new-line  _____|_____|_____  new-line     |     |     new line  board_spots[6]  |  board_spots[7]  |  board_spots[8]  new-line     |     |     ))
+	 (terpri))
+
