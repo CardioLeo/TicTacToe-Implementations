9b6f417d27b7b166cc1d1a811dedd4bde8f97c1e
echocslsk <CardioLeo@gmail.com>
2024-03-06 18:50:00 -0700

    after quite a lot of trying, testing, errors, research, commits changes that define and print a variable in the lisp version of tic tac toe, ha

--- LispTicTacToe/tictactoe.lisp
+++ LispTicTacToe/tictactoe.lisp
@@ -2,8 +2,20 @@
   (print 'tic-tac-toe)
   (terpri))
 
+(defvar *false* 'false)
+(defvar *game_over* *false*)
+(defvar *found_a_match* *false*)
+
 (defun my-ttt-list ()
-  (print '(ttt-global-vars (round_counter mark game_over play_again_input spot_to_fill board_spots)))
+  (print '(ttt-global-vars (round_counter mark play_again_input spot_to_fill board_spots)))
+  (print '"The variable game_over is set to: ")
+  (print *game_over*)
+  (print '"and the varialble found_a_match is set to: ")
+  (print *found_a_match*)  
+  (terpri))
+
+'(defun announce-three ()
+  (or (if (and (= *game_over* 0) (= *found_a_match* (not 0))) (print '(You found three in a row!))) (if (= *game_over* 0) (print '(Keep looking for a match!))))
   (terpri))
 
 (defun my-functions ()
@@ -14,6 +26,7 @@
   (tic-tac-toe)
   (my-ttt-list)
   (my-functions)
+  '(announce-three)
   (print'"
 
 
