630c517a2940a3e3c54a779c56a9f080ed8cb38f
echocslsk <CardioLeo@gmail.com>
2024-03-08 20:09:42 -0700

    tests a lot of basic array stuff, as I am experimenting trying to make sure I know what I am doing when I implement a function effecting an array in the main app

--- array-logic.cpp
+++ array-logic.cpp
@@ -11,23 +11,34 @@
 using namespace std;
 
 void print_spacer() {
-	cout << "\n\n\n\n";
+	cout << "\n\n\n";
 }
 
-void print2() { // Stroustrup's example in latest edition of Tour of C++
-	array<int, 5> v = {88, 95, 57, 92, 32};
+array<int, 5> v = {85, 83, 57, 77, 32};
+
+bool print2(array<int, 5>) { // Stroustrup's example in latest edition of Tour of C++
+	// array<int, 5> v = {85, 83, 57, 77, 32};
 	// char grades[5] = {'A', 'B', 'C', 'D', 'F'};
 
 	// int print_grade_numbers(int v[]) {
-		for (auto x : v) {
+		for (auto &x : v) {
         		cout << x << endl;
-			x++;
+			x = x + 1;
 			cout << x << endl;
-		//return 0;
+		if (x > 90) return true;
 		}
+	return false;
 	// }
 }
 
+bool what_is_bool = print2(v);
+bool print_bool(bool){
+	return what_is_bool;
+}
+
+void change_grade(){
+	v[1] = 95;
+}
 /*
 char print_grade_characters() {
 	for (auto x : grades) {
@@ -39,7 +50,20 @@ char print_grade_characters() {
 }*/
 
 int main(){
-	print2();
+	// print2();
+	print_spacer();
+	what_is_bool = print2(v); // testing original values of v
+	// cout << what_is_bool << "\n"; // testing false value of bool
+	print_bool(what_is_bool);
+	print_spacer();
+	print2(v); // testing incremented values
+	print_spacer();
+	change_grade();
+        what_is_bool = print2(v); // testing that change_grade worked
+	// cout << what_is_bool << "\n"; // testing that bool changed
+        print_bool(what_is_bool);
+	bool what_is_bool2 = print_bool(what_is_bool);
+	cout << what_is_bool2;
 	print_spacer();
 	// print_grade_numbers(grade_numbers[]);
 return 0;
