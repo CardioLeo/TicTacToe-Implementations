bc9b3ab9483097d8b3e7fca7be21472ef37683b8
echocslsk <CardioLeo@gmail.com>
2024-04-18 22:37:24 -0600

    makes several methods public so that main can call them and have a loop which calls methods from both classes of object, both board and details; this is closer to functional, for sure, but I'm not sure if I did that at the expense of making it less memory safe or anyways messing up the intent of classes having private access levels or something else related to abstraction design. It feels like progress with a cost, but I don't yet know what the best real alternative is.

--- withObjects/oop-tictactoe.cpp
+++ withObjects/oop-tictactoe.cpp
@@ -4,23 +4,26 @@ using namespace std;
 
 class Board {
         private:
-                static constexpr std::array<char, 9> board_spots = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+                std::array<char, 9> board_spots = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
         public :
-                static std::array<char,9> give_board_spots(){
-                        return Board::board_spots;
+                std::array<char,9> give_board_spots(){
+                        return board_spots;
                 }
-                static void draw_board(std::array<char,9>){ // this function is fairly self-explanatory; it draws the board by referencing the values from the array, board_spots
+                void draw_board(std::array<char,9>){ // this function is fairly self-explanatory; it draws the board by referencing the values from the array, board_spots
                         cout << "\n\n     |     |     \n";
-                        cout << "  " << Board::board_spots[0] << "  |  " << Board::board_spots[1] << "  |  " << Board::board_spots[2] << "  \n";
+                        cout << "  " << board_spots[0] << "  |  " << board_spots[1] << "  |  " << board_spots[2] << "  \n";
                         cout << "_____|_____|_____\n";
                         cout << "     |     |     \n";
-                        cout << "  " << Board::board_spots[3] << "  |  " << Board::board_spots[4] << "  |  " << Board::board_spots[5] << "  \n";
+                        cout << "  " << board_spots[3] << "  |  " << board_spots[4] << "  |  " << board_spots[5] << "  \n";
                         cout << "_____|_____|_____\n";
                         cout << "     |     |     \n";
-                        cout << "  " << Board::board_spots[6] << "  |  " << Board::board_spots[7] << "  |  " << Board::board_spots[8] << "  \n";
+                        cout << "  " << board_spots[6] << "  |  " << board_spots[7] << "  |  " << board_spots[8] << "  \n";
                         cout << "     |     |     \n\n\n";
                 };
+		void test_change_board_spot(){
+			board_spots[0] = 'X';
+		}
                 /*bool announce_three_in_a_row(){
                         cout << "That's three in a row!\n\nPlayer \" << mark << \" wins!\n";
                         return game_over = true;
@@ -32,24 +35,37 @@ class Board {
 class Details {
         public:
 		// methods
-		int round_count(){
+		int give_round_count(){
 			int r_c = this->round_counter;
 			return r_c;
+		}
+		void increment_round_count(){
+			this->round_counter++;
 		}
                 bool is_game_over(){
 			bool g_o = this->game_over;
 			return g_o;
 		}
-		int test_r_c_is_10(){
-                        return main_game_loop();
-                }
+		//int test_r_c_is_10(){
+                //        return main_game_loop();
+                //}
 		void announce_game_over_value(){
 			cout << "game_over is now set to: " << this->game_over << "\n\n\n";
 		}
-	private:
-	        void what_is_round_num(){
+ 		void what_is_round_num(){
                         cout << "Round Number: " << this->round_counter << "\n\n\n";
                 }
+                void round_limit_checker(){
+                        // should be final place for what_is_round_num();
+                        if (this->round_counter > 9){
+                                this->game_over = true;
+                                announce_game_over_value();
+                                // call or return end game function
+                                // here later
+                        }
+                }
+
+	private:
 		void display_info(){
 			if (this->round_counter == 9){
 				cout << "\n\nLast round!\n\n\n";
@@ -66,6 +82,8 @@ class Details {
 		bool game_over = false;
 
 		// methods
+
+/*
 		void round_limit_checker(){
 			// should be final place for what_is_round_num();
 			if (this->round_counter > 9){
@@ -75,24 +93,31 @@ class Details {
 				// here later
 			}
 		}
+*/
+
+
+/*
                 int main_game_loop(){
 			// round_limit_checker();
-			for (this->round_counter; this->round_counter <= 9; this->round_counter++){
+			for (i = this_game.give_round_count(); i <= 9; this_game.increment_round_count()){
 				what_is_round_num();
-				Board::draw_board(Board::give_board_spots());
+				// board.draw_board(board.give_board_spots());
                         }
 			round_limit_checker();
                         return this->round_counter;
                 }
+		*/
 };
 
 int main(){
 	Details this_game;
-//	Board board;
-//	main_game_loop();
-	// Board::draw_board(Board::give_board_spots());
-		// the following is not intended to be permanent
+	Board board;
 	this_game.announce_game_over_value();
-	this_game.test_r_c_is_10();
+	while (this_game.give_round_count() <= 9){
+		this_game.what_is_round_num();
+		board.draw_board(board.give_board_spots());
+		this_game.increment_round_count();
+	}
+	this_game.round_limit_checker();
 	return 0;
 }
