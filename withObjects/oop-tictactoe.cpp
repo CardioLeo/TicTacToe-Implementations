23419660054061f887fa316555d1f5e90834f7fd
echocslsk <CardioLeo@gmail.com>
2024-04-18 21:44:01 -0600

    makes several related updates; all data members and methods of Board are made to be static; then class Board declaration is moved to top of file so that class Details can call Board::draw_board during the rounds; this is then made to work; unfortunately there is also some uncommited work I did before these last steps, which involves various fairly thorough but basic testing to make sure I knew how to correctly access and change private and public variables in a way that compiles and runs; then I deleted a number of lines that declared and called test vars and funcs; I also renamed one test function within Details class as main_game_loop, which may not be the final place for that function. I might be leaving out a few small changes, but it shouldn't be too bad, ...I hope.

--- withObjects/oop-tictactoe.cpp
+++ withObjects/oop-tictactoe.cpp
@@ -2,6 +2,33 @@
 #include<array>
 using namespace std;
 
+class Board {
+        private:
+                static constexpr std::array<char, 9> board_spots = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+        public :
+                static std::array<char,9> give_board_spots(){
+                        return Board::board_spots;
+                }
+                static void draw_board(std::array<char,9>){ // this function is fairly self-explanatory; it draws the board by referencing the values from the array, board_spots
+                        cout << "\n\n     |     |     \n";
+                        cout << "  " << Board::board_spots[0] << "  |  " << Board::board_spots[1] << "  |  " << Board::board_spots[2] << "  \n";
+                        cout << "_____|_____|_____\n";
+                        cout << "     |     |     \n";
+                        cout << "  " << Board::board_spots[3] << "  |  " << Board::board_spots[4] << "  |  " << Board::board_spots[5] << "  \n";
+                        cout << "_____|_____|_____\n";
+                        cout << "     |     |     \n";
+                        cout << "  " << Board::board_spots[6] << "  |  " << Board::board_spots[7] << "  |  " << Board::board_spots[8] << "  \n";
+                        cout << "     |     |     \n\n\n";
+                };
+                /*bool announce_three_in_a_row(){
+                        cout << "That's three in a row!\n\nPlayer \" << mark << \" wins!\n";
+                        return game_over = true;
+                        // this var needs to be in the game_details
+                        // class.... hm. commenting out this code for now
+                }*/
+};
+
 class Details {
         public:
 		// methods
@@ -13,49 +40,60 @@ class Details {
 			bool g_o = this->game_over;
 			return g_o;
 		}
-
+		int test_r_c_is_10(){
+                        return main_game_loop();
+                }
+		void announce_game_over_value(){
+			cout << "game_over is now set to: " << this->game_over << "\n\n\n";
+		}
 	private:
+	        void what_is_round_num(){
+                        cout << "Round Number: " << this->round_counter << "\n\n\n";
+                }
+		void display_info(){
+			if (this->round_counter == 9){
+				cout << "\n\nLast round!\n\n\n";
+			}
+			cout << "Player Turn: --not yet declared--\n\n\n";
+			what_is_round_num(); // remove this later when
+					     // included in
+					     // round_limit_checker();
+			// here is final place for round_limit_checker();
+		}
+
 		// variables
                 int round_counter = 1;
 		bool game_over = false;
 
-};
-
-class Board {
-	private:
-                std::array<char, 9> board_spots = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        
-	public :
-                std::array<char,9> give_board_spots(){
-                        return board_spots;
+		// methods
+		void round_limit_checker(){
+			// should be final place for what_is_round_num();
+			if (this->round_counter > 9){
+				this->game_over = true;
+				announce_game_over_value();
+				// call or return end game function
+				// here later
+			}
+		}
+                int main_game_loop(){
+			// round_limit_checker();
+			for (this->round_counter; this->round_counter <= 9; this->round_counter++){
+				what_is_round_num();
+				Board::draw_board(Board::give_board_spots());
+				// board::draw_board();
+                        }
+			round_limit_checker();
+                        return this->round_counter;
                 }
-                void draw_board(std::array<char,9>){ // this function is fairly self-explanatory; it draws the board by referencing the values from the array, board_spots
-                        cout << "\n\n     |     |     \n";
-                        cout << "  " << board_spots[0] << "  |  " << board_spots[1] << "  |  " << board_spots[2] << "  \n";
-                        cout << "_____|_____|_____\n";
-                        cout << "     |     |     \n";
-                        cout << "  " << board_spots[3] << "  |  " << board_spots[4] << "  |  " << board_spots[5] << "  \n";
-                        cout << "_____|_____|_____\n";
-                        cout << "     |     |     \n";
-                        cout << "  " << board_spots[6] << "  |  " << board_spots[7] << "  |  " << board_spots[8] << "  \n";
-                        cout << "     |     |     \n\n\n";
-                };
-                /*bool announce_three_in_a_row(){
-                        cout << "That's three in a row!\n\nPlayer \" << mark << \" wins!\n";
-                        return game_over = true;
-			// this var needs to be in the game_details
-			// class.... hm. commenting out this code for now
-                }*/
 };
 
 int main(){
 	Details this_game;
-	Board board;
-	board.draw_board(board.give_board_spots());
+//	Board board;
+//	main_game_loop();
+	// Board::draw_board(Board::give_board_spots());
 		// the following is not intended to be permanent
-	bool game_over = this_game.is_game_over();
-	cout << game_over << "\n\n\n";
-	int round_counter = this_game.round_count();
-	cout << round_counter << "\n\n\n";
+	this_game.announce_game_over_value();
+	this_game.test_r_c_is_10();
 	return 0;
 }
