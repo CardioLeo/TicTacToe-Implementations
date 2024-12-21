ad085c79c631c5d1f62a7fb66152a588c210ee08
CardioLeo <CardioLeo@gmail.com>
2024-12-21 02:59:13 -0700

    adds Game_Data class with some basic attributes, works on making sure that the round counter and the game_over check work more or less correctly

--- withObjects/restart-OOP/oop-tictactoe.cpp
+++ withObjects/restart-OOP/oop-tictactoe.cpp
@@ -21,8 +21,55 @@ class Board {
                 }
 };
 
+class Game_Data {
+        private:
+		bool game_over = false;
+		int current_round = 0;
+		void tell_game_over(){
+			if (this->game_over == true){
+				cout << "The Game is now over\n";
+			} else {
+				cout << "The Game is not yet over; keep playing\n";
+			}
+		}
+		void tell_current_round(){
+			cout << "The current round is: " << this->current_round << "\n\n";
+		}
+		
+		void increment_round(){
+			this->current_round++;
+		}
+
+		void round_check(){
+			this->increment_round();
+			if (this->current_round >= 9){
+				this->game_over = true;
+			}
+		}
+
+        public:
+                void give_game_status(){
+			this->round_check();
+			this->tell_game_over();
+                	this->tell_current_round();
+		}
+
+		bool win_check(){
+			return (this->game_over || this->current_round >= 10);
+		}
+};
+
 int main(){
+	// initializing objects
 	Board board;
+	Game_Data this_game;
+
+	// game play
 	board.draw_board();
+	while (this_game.win_check() == false){
+		board.draw_board();
+		this_game.give_game_status();
+	}
+
 	return 0;
 }
