ce766899c6b646f625b969df490a99aaeec7de16
CardioLeo <CardioLeo@gmail.com>
2024-12-21 14:43:09 -0700

    fixes conversion mistake from last night in lines 125ish and 150ish

--- withObjects/restart-OOP/oop-tictactoe.cpp
+++ withObjects/restart-OOP/oop-tictactoe.cpp
@@ -23,48 +23,112 @@ class Board {
 
 class Game_Data {
         private:
+		
+		// data members
+		
 		bool game_over = false;
 		int current_round = 1;
-		void tell_game_over(){
+		
+		// class methods
+
+		void is_game_over(){
 			if (this->game_over == true){
 				cout << "The Game is now over\n";
 			} else {
 				cout << "The Game is not yet over; keep playing\n";
 			}
 		}
+
+		// round info follows here:
+
+		void last_round_check(){
+			if (this->current_round >= 9){
+				cout << "\n\nLast Round!\n\n\n";
+			}
+		}
 		void tell_current_round(){
 			cout << "The current round is: " << this->current_round << "\n\n";
+			this->last_round_check();
 		}
-		
 		void increment_round(){
 			this->current_round++;
 		}
-
-		void round_check(){
+		void display_round_info(){
+			this->tell_current_round();
 			this->increment_round();
-			// if (this->current_round >= 10){
-			// 	this->game_over = true;
-			// }
 		}
-
         public:
-                void give_game_status(){
-			// this->round_check();
-			this->tell_game_over();
-                	this->tell_current_round();
-			this->increment_round();
+		void display_info(){
+			this->is_game_over();
+			this->display_round_info();
 		}
 
 		bool win_check(){
 			return (this->game_over || this->current_round >= 10);
 		}
+
+		int give_round(){
+			return current_round;
+		}
 };
 
 class Player {
-	public:
-			void announce_turn(){
-				cout << "It is now time to pick a move...\n";
+	private:
+
+		// data members
+
+		char mark = 'Y';
+		int play_again_input = 0;
+		int spot_to_fill = 0;
+		bool is_round_odd = true;
+
+		// class methods
+
+		void say_invalid(){
+			cout << "\nHm, looks like you pressed some key that isn't valid....\n\nPlease try again\n\n" << endl;
+		}
+
+		bool check_input(int spot){
+			if (cin.fail()){
+				this->say_invalid();
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				// must reduce turn counter if false
+				return false;
 			}
+			return true;
+		}
+ 		int get_spot_to_fill(){
+                        int spot = 0;
+                        cin >> spot;
+			bool retake = this->check_input(spot);
+                        return spot;
+                }
+		void announce_turn(){
+			cout << "It is now time to pick a move...\n";
+		}
+
+	public:
+		void compare_round_with_bool(int current_round){
+			if (current_round % 2 == 1){
+				this->is_round_odd = true;	
+			}
+		}
+		void move(){
+				this->announce_turn();
+				this->get_spot_to_fill();
+				// pass spot to board
+			}
+			/* bool spot_error_received(bool error){
+					
+			} */
+	 	void change_turn(int round){
+                        if (round % 2 == 1){
+                                this->mark = 'X';
+                        } else {
+                                this->mark = 'O';
+                        }
+                }
 };
 
 int main(){
@@ -73,14 +137,22 @@ int main(){
 	Game_Data this_game;
 	Player player;
 
-	// game play
+	// game play	
 	board.draw_board();
-	player.announce_turn();
 	while (this_game.win_check() == false){
-		this_game.give_game_status();
+		this_game.display_info();
+		// create a struct of 3-5 values or something
+		// which the board, player, & game pass between each other?
+		// or am I thinking effectively of a global variable again
+		player.move();
+		// player.error_check_and_info_pass();
 		board.draw_board();
-		player.announce_turn();
-	}
+		int round = this_game.give_round();
+		player.change_turn(round);
+		// board.error_check_and_info_pass();
+		// board.find_three_in_a_row();
+		// this_game.change_turn();
+	};
 
 	return 0;
 }
