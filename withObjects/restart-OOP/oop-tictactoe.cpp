274d87369037a24b48dca7753c21780adfc481bf
CardioLeo <CardioLeo@gmail.com>
2024-12-21 02:34:52 -0700

    freshly restarts writing a Cpp using OOP facilities, as I was starting to realize several easier ways to do things; this commit begins with imply a board class and a draw board function, which is run one time and then the program exits

---
+++ withObjects/restart-OOP/oop-tictactoe.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<array>
+#include<limits>
+using namespace std;
+
+class Board {
+	private:
+		std::array<char, 9> board_spots = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+	public:
+		void draw_board(){
+                        cout << "\n\n     |     |     \n";
+                        cout << "  " << this->board_spots[0] << "  |  " << this->board_spots[1] << "  |  " << this->board_spots[2] << "  \n";
+                        cout << "_____|_____|_____\n";
+                        cout << "     |     |     \n";
+                        cout << "  " << this->board_spots[3] << "  |  " << this->board_spots[4] << "  |  " << this->board_spots[5] << "  \n";
+                        cout << "_____|_____|_____\n";
+                        cout << "     |     |     \n";
+                        cout << "  " << this->board_spots[6] << "  |  " << this->board_spots[7] << "  |  " << this->board_spots[8] << "  \n";
+                        cout << "     |     |     \n\n\n";
+                }
+};
+
+int main(){
+	Board board;
+	board.draw_board();
+	return 0;
+}
