94fa9fbf3668e0e092b5d2a72e02acc53041ba8e
CardioLeo <CardioLeo@gmail.com>
2024-12-28 17:30:25 -0700

    prints rudimentary board using three different draw_line functions, vert, val, and horiz

--- GoLang/tictactoe.go
+++ GoLang/tictactoe.go
@@ -5,8 +5,31 @@ import (
 	"fmt"
 )
 
+func draw_vert_line() {
+	fmt.Print("     |     |     \n")
+}
+
+func draw_val_line() {
+        fmt.Print("  a  |  b  |  c  \n")
+}
+
+func draw_horiz_line() {
+	fmt.Print("_____|_____|_____\n")
+}
+
 func draw_board() {
-	fmt.Println("abc\ndef\nghi\n")
+	fmt.Println()
+	draw_vert_line()
+	draw_val_line()
+	draw_horiz_line()
+	draw_vert_line()
+	draw_val_line()
+	draw_horiz_line()
+        draw_vert_line()
+	draw_val_line()
+	draw_vert_line()
+	fmt.Println()
+
 }
 
 func main() {
